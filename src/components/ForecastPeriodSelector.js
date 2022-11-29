57afdee71403cee30a48c04867b27f701fb194bd
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-11-30 00:01:55 +0500

    Изменил названия вкладок

--- src/components/ForecastPeriodSelector.js
+++ src/components/ForecastPeriodSelector.js
@@ -10,13 +10,13 @@ function ForecastPeriodSelector(props) {
         id="forecast-period-selector"
         className="mb-3"
         >
-            <Tab eventKey="hour" title="Час">
+            <Tab eventKey="oneday" title="1 день">
                 <p>Погода на час</p>
             </Tab>
             <Tab eventKey="twodays" title="2 дня">
                 <p>Погода на два дня</p>
             </Tab>
-            <Tab eventKey="week" title="Неделя">
+            <Tab eventKey="fivedays" title="5 дней">
                 <ForecastWeek city={props.city}/>
             </Tab>
         </Tabs>
