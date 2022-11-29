063504597b04417088246f8a0fbeb4c38753f8cb
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-11-29 14:48:30 +0500

    Установил и настроил пакет dotenv-webpack для сохранения ключей от api в файле .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require ('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -17,7 +19,8 @@ module.exports = {
     plugins:[
         new HtmlWebpackPlugin({
             template: "./src/index.html"
-        })
+        }),
+        new Dotenv()
     ],
 
     module: {
