add6583817923caabc2b20ff79fa1040dd4e965d
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-11-29 14:52:20 +0500

    Сделал обращение к api за прогнозом и вывел результат

--- src/components/ForecastWeek.js
+++ src/components/ForecastWeek.js
@@ -1,9 +1,47 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function ForecastWeek() {
-    return (
-        <p>Погода на неделю</p>
-    );
+
+class ForecastWeek extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            weather: {
+                list:[]
+            }
+        };
+    }
+
+    componentDidMount() {
+        const apikey = process.env.REACT_APP_OPWEATHER_APIKEY;
+        const city = 'London';
+        const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apikey}&units=metric`;
+
+        axios.get(url)
+            .then(res => {
+                const weather = res.data;
+                this.setState({ weather });
+            })
+            .catch(error => {
+                console.log(error.response.statusText);
+            });
+    }
+
+    render() {
+        return (
+            <>
+                <p>{console.log(this.state.weather.list)}Погода на неделю</p>
+                <ul>
+                {
+                    this.state.weather.list.map(forecast =>
+                        <li key={forecast.dt}>{forecast.dt_txt + ' -> ' +
+                            forecast.main.temp}</li>
+                    )
+                }
+                </ul>
+            </>
+        );
+    }
 }
 
 export default ForecastWeek;
