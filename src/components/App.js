adcca9b6cc2cb07d78b050860661b9dc39af9939
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-11-30 11:54:11 +0500

    Изменил главный заголовок

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello from React Component</h1>
+                <h1>Прогноз погоды в городе</h1>
                 <CitySelector onCitySelect={this.handleCitySelect}/>
                 {console.log(`Выбрали город ${this.state.city}`)}
                 <ForecastPeriodSelector city={this.state.city}/>
